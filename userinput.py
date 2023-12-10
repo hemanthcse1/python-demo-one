5e53cd0f5ae78275ca1a36387e39ea1523a33697
hemanthcse1 <hemanthcse1@gmail.com>
2023-12-10 17:17:15 +0530

    user input with different data types

---
+++ userinput.py
@@ -0,0 +1,20 @@
+# age = int(input("Enter your age: "))
+#
+# print(type(age))
+# print("your age is:", age)
+
+# birth_year = int(input("Enter you birth year:"))
+# your_name = input("Enter your full name:")
+# your_age = 2023 - birth_year
+# print("Hello", your_name)
+# print("Your current age is:", your_age)
+# print("Welcome to the python programming world!")
+
+
+number = int(input("Enter number:"))
+float_number = float(input("Enter floating number:"))
+name = input("Enter string: ")
+
+print(type(number))
+print(type(float_number))
+print(type(name))
